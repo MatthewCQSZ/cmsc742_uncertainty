7a82c580f77d2f0ec521ff08be97faca9cf54f50
C. Matthew Z <78120468+MatthewCQSZ@users.noreply.github.com>
2023-05-15 22:35:10 -0400

    Add files via upload

--- pets-essential/MPC.py
+++ pets-essential/MPC.py
@@ -253,22 +253,20 @@ class MPC(Controller):
                 train_targ = torch.from_numpy(self.train_targs[batch_idxs]).to(TORCH_DEVICE).float()
 
                 mean, logvar = self.model(train_in, ret_logvar=True)
-                print(mean.shape)
+                #print(mean.shape)
                 train_in = train_in[0]
                 train_in = train_in.detach().cpu().numpy()
-                print("train in shape")
-                print(train_in.shape)
-                train_in = np.hstack((train_in, np.ones((len(train_in), 1))))
-                print(train_in.shape)
-                print("params below")
-                print(self.model.enn_params['mlp/~/linear_0']['w'].shape)
-
-
+                #print("train in shape")
+                #print(train_in.shape)
+                #train_in = np.hstack((train_in, np.ones((len(train_in), 1))))
+                #print(train_in.shape)
+                #print("params below")
+                #print(self.model.enn_params['mlp/~/linear_0']['w'].shape)
                 
-                enn_out = self.model.enn.apply(self.model.enn_params, 
-                                               self.model.enn_state, 
+                enn_out, _ = self.model.enn.apply(self.model.enn_state.params, 
+                                               self.model.enn_state.network_state, 
                                                train_in, 
-                                               next(self.model.rng)) # params, state, inputs, index
+                                               self.model.enn.indexer(next(self.model.rng))) # params, state, inputs, index
                 print(enn_out.shape)
                 
                 # TODO: match inputs size to ENN
